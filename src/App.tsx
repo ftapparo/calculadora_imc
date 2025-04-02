aa01671424c33d53411220c097d2ee7f17ffff30
Flavio Eduardo Tapparo <tapparo.flavio@gmail.com>
2025-04-01 22:15:07 -0300

    Inicio da edição do aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-function App() {
+import { Card, Box, Typography } from '@mui/material';
+
+export default function App() {
 
   return (
-      <div>
-        <p>
-          TESTE
-        </p>
-      </div>
+        <Box
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'flex-start'}
+          width={'100vw'}
+          height={'100vh'}
+        >
+          <Card
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              width: '100%',
+              marginTop: '144px',
+              marginX: '16px'
+            }}
+          >
+
+            {/* Titulo */}
+            <Box
+              marginY={'16px'}
+            >
+              <Typography variant='h5' fontWeight={'700'}>
+                Calculadora IMC
+              </Typography>
+            </Box>
+            
+            {/* Entradas */}
+            <Box>
+              entradas
+            </Box>
+
+            {/* Resultado */}
+            <Box>
+              resultado
+            </Box>
+          </Card>
+        </Box>
   )
 }
-
-export default App
